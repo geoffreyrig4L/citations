65f46cddb997745ff8822d1810ef46be2fef3946
Hulcox <hulcox03@gmail.com>
2024-05-21 11:12:13 +0200

    Create first route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3030;
 
-const routes = require("./routes");
+const quotes = require("./routes/quotes");
+//const user = require("./routes/users");
 
 app.use(express.json());
 
-app.use("/", routes);
+app.use("/quote", quotes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
